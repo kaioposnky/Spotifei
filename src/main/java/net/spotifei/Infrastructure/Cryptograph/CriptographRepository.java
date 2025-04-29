e0ec4d64aaffc979c6a405b92d19d9ee601f71be
Kaio <kposansky@gmail.com>
2025-04-29 18:18:03 -0300

    fix: arrumado comparação de hash nunca funcionando + javadocs
    - Alterado parâmetros de hash e string para comparação
    - Adicionado javadocs básica das funções

--- src/main/java/net/spotifei/Infrastructure/Cryptograph/CriptographRepository.java
+++ src/main/java/net/spotifei/Infrastructure/Cryptograph/CriptographRepository.java
@@ -6,11 +6,21 @@ import de.mkammerer.argon2.Argon2Factory;
 public class CriptographRepository {
     private static final Argon2 argon2 = Argon2Factory.create(32, 16);
 
-    public static String genererateHash(String string){
+    /**
+     * @param string Recebe a string a ser encriptada
+     * @return Retorna a string encriptada
+     */
+    public static String generateHash(String string){
         return argon2.hash(1, 8196, 1, string);
     }
 
+    /**
+     *
+     * @param string Recebe a string a ser checada
+     * @param hash Recebe a string com hash
+     * @return Retorna se a string pertence ao hash
+     */
     public static boolean compareHash(String string, String hash){
-        return argon2.verify(string, hash);
+        return argon2.verify(hash, string);
     }
 }
