0df3e574565e36a9190a20aedf402fc1f139b395
Kaio <kposansky@hotmail.com>
2025-04-30 23:50:06 -0300

    feat: inicio implementação lógica auth
    - lógica para lidar com resposta do banco no services
    - lógica para obter se login foi aceito ou não no controller + exception

--- src/main/java/net/spotifei/Controller/AuthController.java
+++ src/main/java/net/spotifei/Controller/AuthController.java
@@ -1,11 +1,34 @@
 package net.spotifei.Controller;
 
-import net.spotifei.Services.UserService;
+import net.spotifei.Models.Responses.Response;
+import net.spotifei.Services.AuthServices;
+import net.spotifei.Views.JanelaLogin;
+import net.spotifei.Views.MainFrame;
+
+import javax.swing.*;
 
 public class AuthController  {
+    private final JPanel view;
+    private final AuthServices authServices;
+
+    public AuthController(JPanel view) {
+        this.view = view;
+        this.authServices = new AuthServices();
+    }
 
     public void loginUsuario(){
-        UserService userServices = new UserService();
-//        User user = userServices.getUsuario();
+        JanelaLogin loginFrame = (JanelaLogin) view;
+        String email = loginFrame.getTxt_email_login().getText();
+        String password = loginFrame.getTxt_senha_login().getText();
+        Response<Boolean> response = authServices.validateUserLogin(email, password);
+        if (!response.isSuccess()){
+            // mais tarde colocar um logger com error aqui
+            JOptionPane.showMessageDialog(view, response.getMessage());
+            return;
+        }
+        boolean isLoginValid = response.getData();
+        if (isLoginValid){
+            // lógica de mandar para o painel home
+        }
     }
 }
--- src/main/java/net/spotifei/Services/AuthServices.java
+++ src/main/java/net/spotifei/Services/AuthServices.java
@@ -1,8 +1,29 @@
 package net.spotifei.Services;
 
+import net.spotifei.Helpers.ResponseHelper;
+import net.spotifei.Infrastructure.Repository.UserRepository;
+import net.spotifei.Models.Responses.BadResponse;
+import net.spotifei.Models.Responses.Response;
+import net.spotifei.Models.User;
+
+import static net.spotifei.Infrastructure.Cryptograph.CriptographRepository.compareHash;
+
 public class AuthServices {
 
-    public boolean validateUserLogin(String username, String senha){
-        return true;
+    private final UserRepository userRepository = new UserRepository();
+
+    public Response<Boolean> validateUserLogin(String email, String senha){
+        try{
+            User user = userRepository.getUsuarioByEmail(email);
+
+            if (user == null){
+                return ResponseHelper.GenerateBadResponse("Usuário não encontrado!");
+            }
+
+            return ResponseHelper.GenerateSuccessResponse(
+                    "Usuário validado com sucesso!", compareHash(senha, user.getSenha())); // Retorna se a senha encriptada era igual à senha inserida
+        } catch (Exception ex){
+            return ResponseHelper.GenerateErrorResponse(ex.getMessage(), ex);
+        }
     }
 }
