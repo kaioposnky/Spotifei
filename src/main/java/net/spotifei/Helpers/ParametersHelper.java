063a6d05448137cb6cde2377d02c296fe17db91f
Kaio <kposansky@hotmail.com>
2025-05-06 12:06:02 -0300

    fix: corrigido erro onde não era possível traduzir classes com atributos privados
    - alterada forma de obtenção dos fields da classe (getFields para getDeclaredFields)

--- src/main/java/net/spotifei/Helpers/ParametersHelper.java
+++ src/main/java/net/spotifei/Helpers/ParametersHelper.java
@@ -11,7 +11,7 @@ public class ParametersHelper {
         Class<?> clazz = object.getClass();
 
         while (clazz != null){
-            Field[] fields = clazz.getFields();
+            Field[] fields = clazz.getDeclaredFields();
             for(Field field : fields){
                 field.setAccessible(true);
                 try {
