f6ad532034606ff095b4e985e9b583ea82ad3510
Kaio <kposansky@hotmail.com>
2025-05-09 01:23:00 -0300

    feat: adicionada query e código para obter musica por id e resolvido erros
    - adicionada query getMusicById no repository
    - resolvido erro onde parâmetro não era o mesmo que estava no xml
    - resolvido erro onde handler para obter byte array não funcionava

--- src/main/java/net/spotifei/Infrastructure/Repository/MusicRepository.java
+++ src/main/java/net/spotifei/Infrastructure/Repository/MusicRepository.java
@@ -32,7 +32,7 @@ public class MusicRepository {
     public Music getNextMusicOnUserQueue(int userId) throws Exception {
         try{
             Map<String, Object> params = new HashMap<>();
-            params.put("userId", userId);
+            params.put("idUsuario", userId);
 
             String sql = jdbcRepository.getQueryNamed("GetNextMusicOnUserQueueByUserId");
             Music music = jdbcRepository.queryProcedure(sql, params, new BeanHandler<>(Music.class));
@@ -46,13 +46,14 @@ public class MusicRepository {
 
     public byte[] getMusicAsByteArray(int musicId) throws Exception {
         try{
+            ScalarHandler<byte[]> handler = new ScalarHandler<>();
             Map<String, Object> params = new HashMap<>();
             params.put("idMusica", musicId);
 
             String sql = jdbcRepository.getQueryNamed("GetMusicAudioByMusicId");
-            byte[] musicAudio = jdbcRepository.queryProcedure(sql, params, new ScalarHandler<>());
 
-            return musicAudio;
+            // por questões de velocidade o retorno foi juntado com a função que retorna o audio
+            return jdbcRepository.queryProcedure(sql, params, handler);
         } catch (Exception e){
             e.getCause();
             throw e;
@@ -89,6 +90,21 @@ public class MusicRepository {
             int totalMusics = jdbcRepository.queryProcedure(sql, new BeanHandler<>(Integer.class));
 
             return totalMusics;
+        } catch (Exception e){
+            e.getCause();
+            throw e;
+        }
+    }
+
+    public Music getMusicById(int musicId) throws Exception{
+        try{
+            Map<String, Object> params = new HashMap<>();
+            params.put("idMusica", musicId);
+
+            String sql = jdbcRepository.getQueryNamed("GetMusicById");
+            Music music = jdbcRepository.queryProcedure(sql, params, new BeanHandler<>(Music.class));
+
+            return music;
         } catch (Exception e){
             e.getCause();
             throw e;
